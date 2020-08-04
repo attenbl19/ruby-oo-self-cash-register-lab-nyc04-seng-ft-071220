4879f35b01657f0920eb36aa22c0874068b8a89b
Lida Attenborough <attenboroughl@protonmail.com>
2020-08-04 04:09:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,11 @@
 class CashRegister
+  attr_accessor :items, :total, :discount
   def initialize(discount=0)
     @total = 0
     @discount = discount
     items = []
   end
-  
-  def total
-    @total 
-  end
-  
-  def discount
-    @discount
-  end
+
   
   
 end
