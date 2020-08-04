21ad5e42d89f1410aea548959af9e9453e87695b
Lida Attenborough <attenboroughl@protonmail.com>
2020-08-04 03:55:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,14 @@
 class CashRegister
-  def initialize(total)
+  def initialize(total, employee_discount)
     @total = total
+    @employee_discount = employee_discount
   end
   
   def total
     @total
+  end
+  
+  def employee_discount
+    @employee_discount
   end
 end
